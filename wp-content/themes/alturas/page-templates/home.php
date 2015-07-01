5391d92b630524be2e7dfec891eccf3b0d2938f9
Jesse Kevon <jessekevon@gmail.com>
2015-07-01 17:11:01 -0500

    intial commit

---
+++ wp-content/themes/alturas/page-templates/home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+<div class="wrap">
+  <div class="col2 item"></div>
+  <div class="col2 item last"></div>
+  <div class="col3 item"></div>
+  <div class="col3 item"></div>
+  <div class="col3 item last"></div>
+  <div class="col3 item"></div>
+  <div class="col3 item"></div>
+  <div class="col3 item last"></div>
+  <div class="col4 item"></div>
+  <div class="col4 item"></div>
+  <div class="col4 item"></div>
+  <div class="col4 item last"></div>
+</div>
+<?php
+get_footer();
