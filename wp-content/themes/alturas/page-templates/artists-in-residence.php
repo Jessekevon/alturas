ee2f5101fe7acabe931b169ebd39de27cdca77e5
Jesse Kevon <jessekevon@gmail.com>
2015-07-02 17:17:17 -0500

    build out static artists in residence page template

--- wp-content/themes/alturas/page-templates/artists-in-residence.php
+++ wp-content/themes/alturas/page-templates/artists-in-residence.php
@@ -8,19 +8,72 @@
  */
 
 get_header(); ?>
-<div class="container">
-  <div class="col2 item">Test</div>
-  <div class="col2 item"></div>
-  <div class="col3 item"></div>
-  <div class="col3 item"></div>
-  <div class="col3 item"></div>
-  <div class="col3 item"></div>
-  <div class="col3 item"></div>
-  <div class="col3 item"></div>
-  <div class="col4 item"></div>
-  <div class="col4 item"></div>
-  <div class="col4 item"></div>
-  <div class="col4 item"></div>
+<div class="wrap">
+  <div class="intro">
+    <div class="intro-content">
+      <h1>Artists in Residence</h1>
+      <p>The Artist in Residence Program selects artists to explore and reflect their vision of American culture. Alturas Foundation grants support to one artist or artist collective at a time enabling them to examine in-depth their new ideas and creative process.</p>
+    </div>
+  </div>
 </div>
+
+<div class="gray-background">
+  <div class="wrap feature">
+      <div class="col2 feature-block">
+        <div class="feature-block-content">
+          <h1>Laura McPhee</h1>
+          <p>Artist In Residence</p>
+        </div>
+      </div>
+      <div class="col2 feature-block" style="background: url('http://localhost:8888/alturas/wp-content/uploads/2015/07/Snowmobile-Headlights-Valley-Road-Custer-County-Idaho-2004-LoRes.jpg') no-repeat 50% 50%; background-size:cover">
+      </div>
+  </div>
+</div>
+
+<div class="wrap">
+  <div class="col2 intro">
+    <div class="intro-content">
+      <h1>Previous Artists</h1>
+    </div>
+  </div>
+
+  <div class="col2 image-block">
+    <div class="image-block-background-image" style="background: url('http://localhost:8888/alturas/wp-content/uploads/2015/07/Abelardo-Morell-piece-vue-stenope-01.jpg') no-repeat 50% 50%; background-size:cover">
+    </div>
+    <div class="image-block-content">
+      <h1>Abelardo Morell</h1>
+      <p>2013 - 2014</p>
+    </div>
+  </div>
+
+  <div class="col2 image-block">
+    <div class="image-block-background-image" style="background: url('http://localhost:8888/alturas/wp-content/uploads/2015/07/8ac4ace6c3a0a4adb9b8a08ac3c7b3b87fb7d88bc4aee5b8bdaec29dbbb5c29bb9cdbbcac4d288c5c8cd62b2c7ad82b58ac4c6e4c2b7c6aeb9a59493.jpg') no-repeat 50% 50%; background-size:cover">
+    </div>
+    <div class="image-block-content">
+      <h1>Hubbard / Birchler</h1>
+      <p>2012 - 2013</p>
+    </div>
+  </div>
+
+  <div class="col2 image-block">
+    <div class="image-block-background-image" style="background: url('http://localhost:8888/alturas/wp-content/uploads/2015/07/Alturas-Lake-Outlet-Blaine-County-Idaho-2004jpeg.jpg') no-repeat 50% 50%; background-size:cover">
+    </div>
+    <div class="image-block-content">
+      <h1>Laura McPhee</h1>
+      <p>2011 - 2013</p>
+    </div>
+  </div>
+
+  <div class="col2 image-block">
+    <div class="image-block-background-image" style="background: url('http://localhost:8888/alturas/wp-content/uploads/2015/07/CO_Central_Park_Winter_20132.jpg') no-repeat 50% 50%; background-size:cover">
+    </div>
+    <div class="image-block-content">
+      <h1>Abelardo Morell</h1>
+      <p>2009 - 2011</p>
+    </div>
+  </div>
+</div>
+
+
 <?php
 get_footer();
