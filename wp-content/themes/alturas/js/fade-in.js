8bfd5a58a03d6623f7b60fb5f17c0884fdd7261e
Jesse Kevon <jessekevon@gmail.com>
2015-07-22 11:52:42 -0500

    add fade to entire body once document is loaded

--- wp-content/themes/alturas/js/fade-in.js
+++ wp-content/themes/alturas/js/fade-in.js
@@ -7,7 +7,7 @@
 ( function( $ ) {
 	// Add class after page load that adds an opacity change to allow images to fade in smoothly when the page is fully loaded
 	$(window).load(function() {
-	  $(".fadeIn").addClass("fadeIn-1s");
+	  $(".fadeIn, body").addClass("fadeIn-1s");
 	});
 
 } )( jQuery );
